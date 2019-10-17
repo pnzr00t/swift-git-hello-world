4f8438e58330a408bd4f7b76c10837d0a91f95cc
pnzr00t <pnz.r00t@gmail.com>
2019-10-17 18:07:42 +0300

    Initial Commit

---
+++ GitXCodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXCodeTest
+//
+//  Created by 	Oleg2 on 17.10.2019.
+//  Copyright © 2019 Oleg Pustoshkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
